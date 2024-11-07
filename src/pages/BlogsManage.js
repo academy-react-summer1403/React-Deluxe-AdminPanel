48519a65d7168328f6f68e608e91cd6985f8ef2b
ghonche <ataee.ghonche@gmail.com>
2024-11-07 16:35:56 +0330

    BlogsTable

--- src/pages/BlogsManage.js
+++ src/pages/BlogsManage.js
@@ -2,15 +2,18 @@ import { Card, CardHeader, CardBody, CardTitle, CardText } from "reactstrap";
 import RoleCards from "../@core/components/roles-permissions/roles/RoleCards";
 import { Fragment } from "react";
 import UsersList from "../@core/components/user/list";
+import TableStriped from "../@core/components/course/TableStriped";
 
-const SecondPage = () => {
+
+const BlogsManage = () => {
   return (
     <Fragment>
-      <h2>لیست دوره ها</h2>
-      {/* <RoleCards /> */}
-      <UsersList />
+      <h2>لیست  مقالات</h2>
+     
+      <TableStriped />
+
     </Fragment>
   );
 };
 
-export default SecondPage;
+export default BlogsManage;
