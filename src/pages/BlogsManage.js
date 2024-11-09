199274d498ad8bb78cc46a24ba7484ec42aa7ac8
ghonche <ataee.ghonche@gmail.com>
2024-11-10 00:05:14 +0330

    ApiUsers-ApiNews-fixed

--- src/pages/BlogsManage.js
+++ src/pages/BlogsManage.js
@@ -1,12 +1,12 @@
 import { Card, CardHeader, CardBody, CardTitle, CardText } from "reactstrap";
 import RoleCards from "../@core/components/roles-permissions/roles/RoleCards";
 import { Fragment } from "react";
-import UsersList from "../@core/components/user/list";
+import UsersList from "../@core/components/blogs/list/Table";
 
 const BlogsManage = () => {
   return (
     <Fragment>
-      <h2>لیست مقالات</h2>
+      <UsersList />
     </Fragment>
   );
 };
