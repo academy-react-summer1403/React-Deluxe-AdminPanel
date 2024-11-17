c900102e5dab51ed2c75220dd4c80de380a16a71
ghonche <ataee.ghonche@gmail.com>
2024-11-17 13:43:54 +0330

    set-Api-Search-Comments

---
+++ src/core/services/api/Comments.js
@@ -0,0 +1,26 @@
+import http from "../interceptor";
+import { useQuery } from "@tanstack/react-query";
+
+export const useComments = (search) => {
+  const Comments = async () => {
+    try {
+      const query = {};
+      if (search !== "" && search !== null) query.Query = search;
+
+    //   if (role !== "" && role !== null) query.roleId = role;
+
+      const result = await http.get("/Course/CommentManagment", { params: query });
+      console.log(result);
+
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return useQuery({
+    queryKey: ["Comments", search],
+
+    queryFn: Comments,
+  });
+};
