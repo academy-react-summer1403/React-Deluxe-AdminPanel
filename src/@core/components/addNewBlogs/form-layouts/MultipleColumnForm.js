e43f9c24807f4455d28ebb45e604cc9367167451
Shayan <nwryshayan500@gmail.com>
2024-11-19 00:10:35 +0330

    Number2

--- src/@core/components/addNewBlogs/form-layouts/MultipleColumnForm.js
+++ src/@core/components/addNewBlogs/form-layouts/MultipleColumnForm.js
@@ -1,58 +1,122 @@
 // ** Reactstrap Imports
-import { Card, CardHeader, CardTitle, CardBody, Row, Col, Input, Form, Button, Label } from 'reactstrap'
+import { useRef } from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Row,
+  Col,
+  Input,
+  Form,
+  Button,
+  Label,
+} from "reactstrap";
 
 const MultipleColumnForm = () => {
+  // const [formValues, setFormValues] = useState({
+  //   title: "",
+  //   googleTitle: "",
+  // });
+  const formRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Step 2: Collect form data
+    const formData = new FormData(formRef.current);
+    const formValues = Object.fromEntries(formData.entries());
+
+    console.log("Form Submitted Values:", formValues);
+  };
   return (
     <Card>
       <CardHeader>
-        <CardTitle tag='h4'>افزودن خبر جدید</CardTitle>
+        <CardTitle tag="h4">افزودن خبر جدید</CardTitle>
       </CardHeader>
 
       <CardBody>
-        <Form>
+        <Form onSubmit={handleSubmit} innerRef={formRef}>
           <Row>
-            <Col md='6' sm='12' className='mb-1 '>
-              <Label className='form-label' for='nameMulti'>
-عنوان خبر
+            <Col md="6" sm="12" className="mb-1 ">
+              <Label className="form-label" for="nameMulti">
+                عنوان خبر
               </Label>
-              <Input type='text' name='name' id='nameMulti' placeholder='تیتر' />
+              <Input
+                type="text"
+                name="name"
+                id="nameMulti"
+                placeholder="تیتر"
+                // onChange={}
+                // value={}
+              />
             </Col>
-            <Col md='6' sm='12' className='mb-1'>
-              <Label className='form-label' for='lastNameMulti'>
+            <Col md="6" sm="12" className="mb-1">
+              <Label className="form-label" for="lastNameMulti">
                 عنوان گوگل
               </Label>
-              <Input type='text' name='lastname' id='lastNameMulti' placeholder=' لینک' />
+              <Input
+                type="text"
+                name="lastname"
+                id="lastNameMulti"
+                placeholder=" لینک"
+              />
             </Col>
-            <Col md='6' sm='12' className='mb-1'>
-              <Label className='form-label' for='cityMulti'>
-                 توضیحات گوگل
+            <Col md="6" sm="12" className="mb-1">
+              <Label className="form-label" for="cityMulti">
+                توضیحات گوگل
               </Label>
-              <Input type='text' name='city' id='cityMulti' placeholder='توضیحات' />
+              <Input
+                type="text"
+                name="city"
+                id="cityMulti"
+                placeholder="توضیحات"
+              />
             </Col>
-            <Col md='6' sm='12' className='mb-1'>
-              <Label className='form-label' for='CountryMulti'>
+            <Col md="6" sm="12" className="mb-1">
+              <Label className="form-label" for="CountryMulti">
                 توضیح کوتاه
               </Label>
-              <Input type='text' name='country' id='CountryMulti' placeholder='' />
+              <Input
+                type="text"
+                name="country"
+                id="CountryMulti"
+                placeholder=""
+              />
             </Col>
-            <Col md='6' sm='12' className='mb-1'>
-              <Label className='form-label' for='CompanyMulti'>
+            <Col md="6" sm="12" className="mb-1">
+              <Label className="form-label" for="CompanyMulti">
                 کلمات کلیدی
               </Label>
-              <Input type='text' name='company' id='CompanyMulti' placeholder='کلیدی' />
+              <Input
+                type="text"
+                name="company"
+                id="CompanyMulti"
+                placeholder="کلیدی"
+              />
             </Col>
-            <Col md='6' sm='12' className='mb-1'>
-              <Label className='form-label' for='EmailMulti'>
+            <Col md="6" sm="12" className="mb-1">
+              <Label className="form-label" for="EmailMulti">
                 دسته بندی خبر
               </Label>
-              <Input type='email' name='Email' id='EmailMulti' placeholder='اخبار پژوهشگاه' />
+              <Input
+                type="text"
+                name="Email"
+                id="EmailMulti"
+                placeholder="اخبار پژوهشگاه"
+              />
             </Col>
-            <Col sm='12'>
-              <div className='d-flex'>
-                <Button className='me-1' color='primary' type='submit' onClick={e => e.preventDefault()}>
+            <Col sm="12">
+              <div className="d-flex">
+                <Button
+                  className="me-1"
+                  color="primary"
+                  type="submit"
+                  // onClick={(e) => e.preventDefault()}
+                >
                   ثبت
                 </Button>
-                <Button outline color='secondary' type='reset'>
+                <Button outline color="secondary" type="reset">
                   پاک کردن همه
                 </Button>
               </div>
@@ -61,6 +125,6 @@ const MultipleColumnForm = () => {
         </Form>
       </CardBody>
     </Card>
-  )
-}
-export default MultipleColumnForm
+  );
+};
+export default MultipleColumnForm;
